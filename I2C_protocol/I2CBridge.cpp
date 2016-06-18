554a3ff8b10559d9b91d391ced8db362df57c467
Patrik Baranišin <kingofwolwes@gmail.com>
2016-06-18 15:47:08 +0200

    test i2c update

--- I2C_protocol/I2CBridge.cpp
+++ I2C_protocol/I2CBridge.cpp
@@ -126,9 +126,11 @@ int main(int argc, char *argv [])
 			case BOUT:
 				SwitchServer* s = (SwitchServer*) ImplementedResourceTypes::createServerOfType(OIC_SWITCH_TYPE, name);
 				s->setI2CDevice(uno3,res.first);
-				cout << uno3.readData(res.first) << endl;
-				cout << (int) uno3.getResourceLogic(res.first) << endl;
-				s->setInitialState(uno3.readData(res.first) == 1);
+				bool isOn = uno3.readData(res.first) == 1;
+				if(uno3.getResourceLogic(res.first) == INVERTED){
+					isOn = uno3.readData(res.first) == 0;
+				}
+				s->setInitialState(isOn);
 				servers.push_back(s);
 				break;
 		}
