061ecc507679de0252725d62354ed3f1f72547ef
Patrik Baranišin <kingofwolwes@gmail.com>
2016-06-18 17:08:08 +0200

    test i2c update

--- src/SwitchResource.cpp
+++ src/SwitchResource.cpp
@@ -21,16 +21,17 @@ void SwitchResource::onAttrSet(const RCSResourceAttributes &attrs, int eCode) {
 
 void SwitchResource::onAttrGet(const RCSResourceAttributes &attrs, int eCode) {
     cout << "----GET---- State: " << attrs.at(SwitchServer::IS_ON_ATTR).toString() << "-----------" << endl;
-    isOn = attrs.at(SwitchServer::IS_ON_ATTR).toString() == "true";
+
 }
 
 void SwitchResource::onCacheUpdated(const RCSResourceAttributes &attrs) {
-
+    isWaitingForUpdate = false;
     cout << "----UPDATED---- State: " << attrs.at(SwitchServer::IS_ON_ATTR).toString() << "-----------" << endl;
-    cout << "----UPDATED---- Resource: " << getAbsoluteUri() << "-----------" << endl;
+    isOn = attrs.at(SwitchServer::IS_ON_ATTR).toString() == "true";
     for(OnAttrChangeListener* listener : listeners){
         listener->onAttrChanged();
     }
+
 }
 
 void SwitchResource::callService(const string &service) {
@@ -53,26 +54,30 @@ void SwitchResource::callService(const string &service) {
 }
 
 void SwitchResource::turnOn() {
-    isOn = true;
+    waitForUpdate();
     RCSResourceAttributes attribute;
     attribute[SwitchServer::IS_ON_ATTR] = true;
     resource->setRemoteAttributes(attribute, setCallback);
+    isWaitingForUpdate = true;
 }
 
 void SwitchResource::turnOff() {
-    isOn = false;
+    waitForUpdate();
     RCSResourceAttributes attribute;
     attribute[SwitchServer::IS_ON_ATTR] = false;
     resource->setRemoteAttributes(attribute, setCallback);
+    isWaitingForUpdate = true;
 }
 
 SwitchResource::SwitchResource(ResourceArgumentsBundle args) {
-    isOn = false;
     init(args.resources.back());
     resource->getRemoteAttributes(getCallback);
+    cout << "......."  << isOn << endl;
+    this_thread::sleep_for(chrono::milliseconds(MIN_RANGE_TO_WAIT));
 }
 
 int SwitchResource::getState() {
+    waitForUpdate();
     return isOn;
 }
 
