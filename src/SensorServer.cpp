61404ee6258f85cd79306a2cb1fdf10dbff3eaca
Patrik Baranišin <kingofwolwes@gmail.com>
2016-06-20 13:35:11 +0200

    test i2c update

--- src/SensorServer.cpp
+++ src/SensorServer.cpp
@@ -63,6 +63,8 @@ RCSSetResponse SensorServer::onSetRequest(const RCSRequest& req, RCSResourceAttr
 void SensorServer::onEvent(int newValue) {
     cout <<  "OnEvent listener: "  << newValue <<endl;
     resource->setAttribute(IS_ON_ATTR, (bool) newValue);
+    this_thread::sleep_for(chrono::milliseconds(500));
+    cout <<  "OnEvent listener get value: "  << resource->getAttribute(IS_ON_ATTR) <<endl;
 }
 
 
